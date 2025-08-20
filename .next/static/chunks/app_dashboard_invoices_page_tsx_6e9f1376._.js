03820d78b55db1ab24ad22f4b475280ee6f55b7f
devjoshi2004 <dev.inventam@gmail.com>
2025-08-20 10:47:12 +0530

    Update Next.js build artifacts, add new components for dashboard invoices and search functionality

---
+++ .next/static/chunks/app_dashboard_invoices_page_tsx_6e9f1376._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c59a1152._.js",
+  "static/chunks/app_ui_search_tsx_aa2e6a1b._.js"
+],
+    source: "dynamic"
+});
