6aca60e9f29cb905af05b03560d4de99b5aa2ab7
devjoshi2004 <dev.inventam@gmail.com>
2025-08-20 15:44:10 +0530

    Refactor updateInvoice function to include validation and error handling: enhance form state management in EditInvoiceForm component and update action binding.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,7 +13,6 @@ export type State = {
   };
   message?: string | null;
 };
- 
 
 const sql = postgres(process.env.POSTGRES_PRISMA_URL!, { ssl: "require" });
 
@@ -34,8 +33,8 @@ const FormSchema = z.object({
 // Create Invoice Api
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(prevState: State,   formData: FormData) {
-  const validatedFields = CreateInvoice.safeParse({  
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get(`customerId`),
     amount: formData.get(`amount`),
     status: formData.get(`status`),
@@ -67,27 +66,39 @@ export async function createInvoice(prevState: State,   formData: FormData) {
 // Update Invoice Api
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
   });
-
+ 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+ 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
-
+ 
   try {
     await sql`
-  UPDATE invoices
-  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-  WHERE id = ${id}
-  `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
   } catch (error) {
-    console.log(error);
+    return { message: 'Database Error: Failed to Update Invoice.' };
   }
-
-  revalidatePath("/dashboard/invoices");
-  // redirect("/dashboard/invoices");
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 // Delete Invoice Api
