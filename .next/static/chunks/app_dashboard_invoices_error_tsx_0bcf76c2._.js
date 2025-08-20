6f485bdbf7a70ec35e46aa4d3d14e58a22f325d3
devjoshi2004 <dev.inventam@gmail.com>
2025-08-20 14:29:21 +0530

    Refactor invoice management: add error handling in delete action, implement not-found page for invoice editing, and enhance logging for invoice fetching.

--- .next/static/chunks/app_dashboard_invoices_error_tsx_0bcf76c2._.js
+++ .next/static/chunks/app_dashboard_invoices_error_tsx_0bcf76c2._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_75ced941._.js"
+  "static/chunks/app_dashboard_invoices_error_tsx_7e72c89d._.js"
 ],
     source: "dynamic"
 });
