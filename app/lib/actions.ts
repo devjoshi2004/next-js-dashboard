c554d4039c6f43092f1e7e5a6a0d16a6cd5f15aa
devjoshi2004 <dev.inventam@gmail.com>
2025-08-20 14:00:16 +0530

    Add invoice management features: implement create, update, and delete actions with error handling in server actions, and enhance UI with loading skeletons and pagination.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,23 +8,17 @@ import { redirect } from "next/navigation";
 const sql = postgres(process.env.POSTGRES_PRISMA_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(["pending", "paid"]),
-    date: z.string(),
-})
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
 
 // Create Invoice Api
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  };
-
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get(`customerId`),
     amount: formData.get(`amount`),
@@ -34,10 +28,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -55,14 +53,18 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
   UPDATE invoices
   SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
   WHERE id = ${id}
   `;
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+  // redirect("/dashboard/invoices");
 }
 
 // Delete Invoice Api
