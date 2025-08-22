c7ef31f2b89930de79dafbb34e213afa68e5cd28
devjoshi2004 <dev.inventam@gmail.com>
2025-08-22 10:24:03 +0530

    add susspence wrapper on customer page

--- .next/static/chunks/app_dashboard_invoices_page_tsx_6e9f1376._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_c59a1152._.js",
-  "static/chunks/app_ui_search_tsx_aa2e6a1b._.js"
-],
-    source: "dynamic"
-});
